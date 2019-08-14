5239a85243562674045e0cb99f74b11b1515fdc1
Guilherme Bernal <guilherme@cubos.io>
2019-08-14 07:56:55 -0300

    Remove test code

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,2 @@
-import { Parser } from "@sdkgen/parser";
-import { writeFileSync } from "fs";
-import { generateDartClientSource } from "./dart-client";
-
 export * from "./dart-client";
 
-
-writeFileSync("src/client.dart", generateDartClientSource(new Parser("src/api.sdkgen").parse(), {}));
