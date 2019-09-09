6fcbf05c8af868c51d7281716c27de6cbb34b51b
Guilherme Bernal <guilherme@cubos.io>
2019-09-09 09:26:07 -0300

    Fix for CheckMultipleDeclarationVisitor

--- src/semantic/1_check_multiple_declaration.ts
+++ src/semantic/1_check_multiple_declaration.ts
@@ -8,8 +8,8 @@ export class CheckMultipleDeclarationVisitor extends Visitor {
     visit(node: AstNode) {
         if (node instanceof TypeDefinition) {
             const previousType = this.nameToType.get(node.name);
-            if (previousType && JSON.stringify(previousType) !== JSON.stringify(node)) {
-                throw new SemanticError(`Type '${node.name}' at ${node.location} is defined multiple times`);
+            if (previousType && JSON.stringify(previousType) !== JSON.stringify(node.type)) {
+                throw new SemanticError(`Type '${node.name}' at ${node.location} is defined multiple times (also at ${previousType.location})`);
             }
             this.nameToType.set(node.name, node.type);
         }
