3137f1dad2cd429270b999dd40db044e40741a89
Guilherme Bernal <guilherme@cubos.io>
2019-09-13 14:09:17 -0300

    improve typecheck on bytes

--- src/encode-decode.ts
+++ src/encode-decode.ts
@@ -14,54 +14,54 @@ function simpleEncodeDecode(path: string, type: string, value: any) {
         }
     } else if (type === "bool") {
         if (typeof value !== "boolean") {
-            throw new Error(`Invalid Type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
+            throw new Error(`Invalid type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
         }
         return value;
     } else if (simpleStringTypes.includes(type)) {
         if (typeof value !== "string") {
-            throw new Error(`Invalid Type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
+            throw new Error(`Invalid type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
         }
         return value;
     } else if (type === "hex") {
         if (typeof value !== "string" || !value.match(/^(?:[A-Fa-f0-9]{2})*$/)) {
-            throw new Error(`Invalid Type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
+            throw new Error(`Invalid type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
         }
         return value.toLowerCase();
     } else if (type === "uuid") {
         if (typeof value !== "string" || !value.match(/^[A-Fa-f0-9]{8}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{12}$/)) {
-            throw new Error(`Invalid Type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
+            throw new Error(`Invalid type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
         }
         return value.toLowerCase();
     } else if (type === "base64") {
         if (typeof value !== "string"/* || !value.match(/^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$/)*/) {
-            throw new Error(`Invalid Type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
+            throw new Error(`Invalid type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
         }
         return value;
     } else if (type === "int") {
         if (typeof value !== "number" || (value | 0) !== value) {
-            throw new Error(`Invalid Type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
+            throw new Error(`Invalid type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
         }
         return value;
     } else if (type === "uint") {
         if (typeof value !== "number" || (value | 0) !== value || value < 0) {
-            throw new Error(`Invalid Type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
+            throw new Error(`Invalid type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
         }
         return value;
     } else if (type === "float") {
         if (typeof value !== "number") {
-            throw new Error(`Invalid Type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
+            throw new Error(`Invalid type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
         }
         return value;
     } else if (type === "money") {
         if (typeof value !== "number" || !Number.isInteger(value)) {
-            throw new Error(`Invalid Type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
+            throw new Error(`Invalid type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
         }
         return value;
     } else if (type === "void") {
         return null;
     } else if (type === "latlng") {
         if (typeof value !== "object" || typeof value.lat !== "number" || typeof value.lng !== "number") {
-            throw new Error(`Invalid Type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
+            throw new Error(`Invalid type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
         }
         return value;
     } else {
@@ -72,12 +72,12 @@ function simpleEncodeDecode(path: string, type: string, value: any) {
 export function encode(typeTable: TypeTable, path: string, type: TypeDescription, value: any): any {
     if (Array.isArray(type)) {
         if (!type.includes(value)) {
-            throw new Error(`Invalid Type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
+            throw new Error(`Invalid type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
         }
         return value;
     } else if (typeof type === "object") {
         if (typeof value !== "object" || value === "undefined") {
-            throw new Error(`Invalid Type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
+            throw new Error(`Invalid type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
         }
         const obj: any = {};
         for (const key in type) {
@@ -91,24 +91,24 @@ export function encode(typeTable: TypeTable, path: string, type: TypeDescription
             return encode(typeTable, path, type.slice(0, type.length - 1), value);
     } else if (type.endsWith("[]")) {
         if (!Array.isArray(value)) {
-            throw new Error(`Invalid Type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
+            throw new Error(`Invalid type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
         }
         return value.map((entry, index) => encode(typeTable, `${path}[${index}]`, type.slice(0, type.length - 2), entry));
     } else if (simpleTypes.includes(type)) {
         return simpleEncodeDecode(path, type, value);
     } else if (type === "bytes") {
         if (!(value instanceof Buffer)) {
-            throw new Error(`Invalid Type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
+            throw new Error(`Invalid type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
         }
         return value.toString("base64");
     } else if (type === "date") {
         if (!(value instanceof Date)) {
-            throw new Error(`Invalid Type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
+            throw new Error(`Invalid type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
         }
         return value.toISOString().split("T")[0];
     } else if (type === "datetime") {
         if (!(value instanceof Date)) {
-            throw new Error(`Invalid Type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
+            throw new Error(`Invalid type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
         }
         return value.toISOString().replace("Z", "");
     } else {
@@ -124,12 +124,12 @@ export function encode(typeTable: TypeTable, path: string, type: TypeDescription
 export function decode(typeTable: TypeTable, path: string, type: TypeDescription, value: any): any {
     if (Array.isArray(type)) {
         if (!type.includes(value)) {
-            throw new Error(`Invalid Type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
+            throw new Error(`Invalid type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
         }
         return value;
     } else if (typeof type === "object") {
         if (typeof value !== "object" || value === "undefined") {
-            throw new Error(`Invalid Type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
+            throw new Error(`Invalid type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
         }
         const obj: any = {};
         for (const key in type) {
@@ -143,24 +143,28 @@ export function decode(typeTable: TypeTable, path: string, type: TypeDescription
             return decode(typeTable, path, type.slice(0, type.length - 1), value);
     } else if (type.endsWith("[]")) {
         if (!Array.isArray(value)) {
-            throw new Error(`Invalid Type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
+            throw new Error(`Invalid type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
         }
         return value.map((entry, index) => decode(typeTable, `${path}[${index}]`, type.slice(0, type.length - 2), entry));
     } else if (simpleTypes.includes(type)) {
         return simpleEncodeDecode(path, type, value);
     } else if (type === "bytes") {
-        if (typeof value !== "string"/* || !value.match(/^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$/)*/) {
-            throw new Error(`Invalid Type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
+        if (typeof value !== "string") {
+            throw new Error(`Invalid type at '${path}', expected ${type} (base64), got ${JSON.stringify(value)}`);
         }
-        return Buffer.from(value, "base64");
+        const buffer = Buffer.from(value, "base64");
+        if (buffer.toString("base64") !== value) {
+            throw new Error(`Invalid type at '${path}', expected ${type} (base64), got ${JSON.stringify(value)}`);
+        }
+        return buffer
     } else if (type === "date") {
         if (typeof value !== "string" || !value.match(/^[0-9]{4}-[01][0-9]-[0123][0-9]$/)) {
-            throw new Error(`Invalid Type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
+            throw new Error(`Invalid type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
         }
         return new Date(parseInt(value.split("-")[0], 10), parseInt(value.split("-")[1], 10) - 1, parseInt(value.split("-")[2], 10));
     } else if (type === "datetime") {
         if (typeof value !== "string" || !value.match(/^[0-9]{4}-[01][0-9]-[0123][0-9]T[012][0-9]:[0123456][0-9]:[0123456][0-9](\.[0-9]{1,6})?Z?$/)) {
-            throw new Error(`Invalid Type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
+            throw new Error(`Invalid type at '${path}', expected ${type}, got ${JSON.stringify(value)}`);
         }
         return new Date(value + "Z");
     } else {
