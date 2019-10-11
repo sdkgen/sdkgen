6596ab417c0a7ae23bd2f829251d69363012440f
Guilherme <guilherme@cubos.io>
2019-10-11 12:33:19 -0300

    Fix compactibility error text

--- src/compatibility/index.ts
+++ src/compatibility/index.ts
@@ -79,7 +79,7 @@ function checkClientToServer(path: string, issues: string[], t1: Type, t2: Type)
     if (t1 instanceof EnumType && t2 instanceof EnumType) {
         for (const value of t1.values) {
             if (!t2.values.map(v => v.value).includes(value.value)) {
-                issues.push(`The enum at ${path} used to accept the value "${value}" that doesn't exist now. Clients that send it will fail.`);
+                issues.push(`The enum at ${path} used to accept the value "${value.value}" that doesn't exist now. Clients that send it will fail.`);
             }
         }
         return;
@@ -168,7 +168,7 @@ function checkServerToClient(path: string, issues: string[], t1: Type, t2: Type)
     if (t1 instanceof EnumType && t2 instanceof EnumType) {
         for (const value of t2.values) {
             if (!t1.values.map(v => v.value).includes(value.value)) {
-                issues.push(`The enum at ${path} now has the value "${value}" that didn't exist before. Client will crash if it receives it`);
+                issues.push(`The enum at ${path} now has the value "${value.value}" that didn't exist before. Client will crash if it receives it`);
             }
         }
         return;
