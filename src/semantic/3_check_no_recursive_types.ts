36ab2d4f902717006e94f216dd3b279c28824511
Guilherme <guilherme@cubos.io>
2019-10-13 15:24:59 -0300

    Fix CheckNoRecursiveTypesVisitor

--- src/semantic/3_check_no_recursive_types.ts
+++ src/semantic/3_check_no_recursive_types.ts
@@ -1,20 +1,24 @@
-import { AstNode, Field, TypeDefinition, TypeReference } from "../ast";
+import { AstNode, Field, Type, TypeDefinition, TypeReference } from "../ast";
 import { SemanticError } from "./analyser";
 import { Visitor } from "./visitor";
 
 export class CheckNoRecursiveTypesVisitor extends Visitor {
     path: string[] = [];
+    rootType: Type | undefined;
 
     visit(node: AstNode) {
         if (node instanceof TypeDefinition) {
             this.path = [node.name];
+            this.rootType = node.type;
             super.visit(node);
+            this.path = [];
+            this.rootType = undefined;
         } else if (node instanceof Field) {
             this.path.push(node.name);
             super.visit(node);
             this.path.pop();
         } else if (node instanceof TypeReference) {
-            if (this.path[0] === node.name) {
+            if (this.rootType === node.type) {
                 throw new SemanticError(`Detected type recursion: ${this.path.join(".")} at ${node.location}`);
             }
             this.visit(node.type);
