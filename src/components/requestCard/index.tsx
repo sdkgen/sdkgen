51b42cd5005edaa4ee2e72ecb8af7c8ebe22a70a
Kevin Oliveira <kevin.oliveira@live.com>
2019-10-14 10:13:03 -0300

    request card bottom

--- src/components/requestCard/index.tsx
+++ src/components/requestCard/index.tsx
@@ -1,13 +1,25 @@
 import * as React from "react";
 import s from "./requestCard.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronDown, faChevronUp, faLink } from "@fortawesome/free-solid-svg-icons";
+import {
+	faChevronDown,
+	faChevronUp,
+	faLink,
+	faPlay,
+	faPause,
+	faRedo,
+} from "@fortawesome/free-solid-svg-icons";
 import MonacoEditor from "react-monaco-editor";
+import classNames from "classnames";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+
+type RequestStatus = "notFetched" | "fetching" | "error" | "sucess" | "timeout";
 
 export const RequestCard = Card;
 
 function Card() {
 	const [open, setOpen] = React.useState<boolean>(false);
+	const [requestStatus, setRequestStatus] = React.useState<RequestStatus>("notFetched");
 
 	if (!open)
 		return (
@@ -29,6 +41,11 @@ function Card() {
 				</div>
 				<FontAwesomeIcon size="xs" icon={faChevronUp} className={s.icon} />
 			</div>
+			<div className={s.subHeader}>
+				<div className={s.tab}>Arguments</div>
+				<div className={classNames(s.tab, s.active)}>Response</div>
+				<div className={s.tab}>Extra Information</div>
+			</div>
 			<div className={s.content}>
 				<MonacoEditor
 					// width="800"
@@ -47,7 +64,57 @@ function Card() {
 					// editorDidMount={::this.editorDidMount}
 				/>
 			</div>
-			<div className={s.bottom}>bottom</div>
+			<Bottom
+				status={requestStatus}
+				onClick={() => {
+					const a: Record<RequestStatus, RequestStatus> = {
+						notFetched: "fetching",
+						fetching: "error",
+						error: "sucess",
+						sucess: "timeout",
+						timeout: "notFetched",
+					};
+					setRequestStatus(a[requestStatus]);
+				}}
+			/>
+		</div>
+	);
+}
+
+interface BottomProps {
+	onClick: () => void;
+	status: RequestStatus;
+}
+function Bottom(props: BottomProps) {
+	const icons: Record<RequestStatus, IconProp> = {
+		notFetched: faPlay,
+		fetching: faPause,
+		error: faRedo,
+		sucess: faRedo,
+		timeout: faRedo,
+	};
+	const labels: Record<RequestStatus, string> = {
+		notFetched: "Make Request",
+		fetching: "Fetching",
+		error: "Error, Retry?",
+		sucess: "Sucess, Retry?",
+		timeout: "Time out, Retry?",
+	};
+	const colors: Record<RequestStatus, string> = {
+		notFetched: s.blue,
+		fetching: s.orange,
+		error: s.red,
+		sucess: s.green,
+		timeout: s.purple,
+	};
+	const selectedIcon = icons[props.status];
+	const selectedLabel = labels[props.status];
+	const classModifier = colors[props.status];
+
+	return (
+		<div className={classNames(s.bottom, classModifier)} onClick={props.onClick}>
+			<div className={s.label}>{selectedLabel}</div>
+			<FontAwesomeIcon size="xs" icon={selectedIcon} className={s.icon} />
 		</div>
 	);
 }
