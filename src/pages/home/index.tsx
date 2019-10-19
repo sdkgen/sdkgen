34e38fc2791a4d69cf45f0c73185ac32ac28b952
Kevin Oliveira <kevin.oliveira@live.com>
2019-10-19 20:10:06 -0300

    debounce

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,15 +3,18 @@ import { observer } from "mobx-react-lite";
 import s from "./home.scss";
 import { SearchInput } from "components/searchInput";
 import { RequestCard } from "components/requestCard";
+import { useDebounce } from "use-debounce";
 import RootStore from "stores";
 
 function Home() {
 	const { requestsStore } = React.useContext(RootStore);
 	const [searchString, setSearchString] = React.useState<string>("");
+	const [searchStringDebounced] = useDebounce(searchString, 500);
+
 	const { api } = requestsStore;
 	const Cards = Object.entries(api)
 		.filter(([fnName, _]) =>
-			fnName.toLocaleLowerCase().includes(searchString.toLocaleLowerCase()),
+			fnName.toLocaleLowerCase().includes(searchStringDebounced.toLocaleLowerCase()),
 		)
 		.map(([fnName, FnModel]) => {
 			return <RequestCard key={fnName} model={FnModel} />;
