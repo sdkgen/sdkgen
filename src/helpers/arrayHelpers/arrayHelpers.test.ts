88516dbdbe00c0eda3df2ad213f4c2cd7ec7f888
Kevin Oliveira <kevin.oliveira@live.com>
2019-10-26 18:02:20 -0300

    working simple ts test

---
+++ src/helpers/arrayHelpers/arrayHelpers.test.ts
@@ -0,0 +1,36 @@
+import { sample } from ".";
+
+test("basic", () => {
+	const alphabet = [
+		"A",
+		"B",
+		"C",
+		"D",
+		"E",
+		"F",
+		"G",
+		"H",
+		"I",
+		"J",
+		"K",
+		"L",
+		"M",
+		"N",
+		"O",
+		"P",
+		"Q",
+		"R",
+		"S",
+		"T",
+		"U",
+		"V",
+		"W",
+		"X",
+		"Y",
+		"Z",
+	];
+
+	const sampleOfAlphabet = sample(alphabet);
+
+	expect(alphabet).toContain(sampleOfAlphabet);
+});
