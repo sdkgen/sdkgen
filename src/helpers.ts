47983fa1366f5a9264ebbc2f47923c8e5dc24bc3
Douglas Gadêlha <dgadelha@live.com>
2019-11-14 18:46:12 -0300

    round doubles to int when needed

--- src/helpers.ts
+++ src/helpers.ts
@@ -17,8 +17,10 @@ export function cast(value: string, type: Type): string {
         return `(${value} as List).map((e) => ${cast("e", (type as ArrayType).base)}).toList()`;
     } else if (type.constructor.name === "VoidPrimitiveType") {
         return value;
+    } else if (type.constructor.name === "IntPrimitiveType" || type.constructor.name === "UIntPrimitiveType" || type.constructor.name === "MoneyPrimitiveType") {
+        return `${value}.round()`;
     } else {
-        return `${value} as ${generateTypeName(type)}`
+        return `${value} as ${generateTypeName(type)}`;
     }
 }
 
