201e727fcec052ffa9aca11d9f68086623311759
Guilherme Bernal <guilherme@cubos.io>
2019-12-13 17:29:16 -0300

    Remove unneeded BaseApiConfig constructor

--- src/api-config.ts
+++ src/api-config.ts
@@ -5,12 +5,6 @@ export abstract class BaseApiConfig<ExtraContextT = {}> {
     astJson!: AstJson;
     private _ast: AstRoot | null = null;
 
-    constructor() {
-        for (const name of Object.keys(this.astJson.functionTable)) {
-            this.fn[name] = async () => { this.err.Fatal(`Function '${name}' is not implemented`); };
-        }
-    }
-
     get ast() {
         if (!this._ast)
             this._ast = jsonToAst(this.astJson);
