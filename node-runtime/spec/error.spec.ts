9a94f92779f609b2e8258d43f5448ba7062c6881
Guilherme Bernal <guilherme@cubos.io>
2020-02-21 23:18:40 -0300

    add error spec

---
+++ node-runtime/spec/error.spec.ts
@@ -0,0 +1,12 @@
+import { SdkgenError } from "../src";
+
+class TestError extends SdkgenError {}
+
+describe("Error", () => {
+    test("SdkgenError behaves as expected", () => {
+        const err1 = new TestError("the error message");
+        expect(err1.message).toEqual("the error message");
+        expect(err1.type).toEqual("TestError");
+        expect(JSON.parse(JSON.stringify(err1))).toEqual({ type: "TestError", message: "the error message" });
+    });
+});
