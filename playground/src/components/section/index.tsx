85490b13a2ef1e1918f6f1875130def67c3bfc9a
kevin oliveira <kevin@cubos.io>
2020-10-04 00:30:17 -0300

    bookmark endpoints
    this feature allows the user to bookmark endpoints, displaying those endpoints on top of all other endpoints. the status of bookmarked is persisted on localstorage.

---
+++ playground/src/components/section/index.tsx
@@ -0,0 +1,21 @@
+
+import * as React from "react";
+import classnames from "classnames";
+import s from "./section.scss";
+
+interface TitleProps {
+	title: string
+	featured?: boolean;
+}
+
+export function Section(props: React.PropsWithChildren<TitleProps>) {
+
+	const { title, featured, children } = props;
+
+	return (
+		<div className={classnames(s.section, featured && s.featured)}>
+			<div className={s.title}>{title}</div>
+			<div className={s.childrenWrapper}>{children}</div>
+		</div>
+	);
+}
