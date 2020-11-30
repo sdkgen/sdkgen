sdkgen/sdkgen

commit b88ccd9951b8a6af5493fb2513a635c250c22228

feat: transfer arbitrary data with a sdkgen error (#133)

browser-runtime/src/http-client.ts
@@ -113,19 +113,22 @@ export class SdkgenHttpClient {
       };
 
       req.send(JSON.stringify(request));
-    }).catch(err => {
-      const errClass = this.errClasses[err.type];
+    }).catch(error => {
+      const errClass = this.errClasses[error.type];
 
       if (errClass) {
-        throw new errClass(err.message);
-      } else {
-        throw err;
+        const errorJson = this.astJson.errors.find(err => (Array.isArray(err) ? err[0] === error.type : err === error.type));
+
+        if (errorJson) {
+          if (Array.isArray(errorJson)) {
+            throw new errClass(error.message, decode(this.astJson.typeTable, `${errClass.name}.data`, errorJson[1], error.data));
+          } else {
+            throw new errClass(error.message, undefined);
+          }
+        }
       }
+
+      throw new this.errClasses.Fatal(`${error.type}: ${error.message}`, undefined);
     });
-
-    const ret = decode(this.astJson.typeTable, `${functionName}.ret`, func.ret, encodedRet);
-
-    this.successHook(ret, functionName, args);
-    return ret;
   }
 }

node-runtime/src/http-client.ts
@@ -6,10 +6,10 @@ import { hostname } from "os";
 import { URL } from "url";
 import { Context } from "./context";
 import { decode, encode } from "./encode-decode";
-import { SdkgenError } from "./error";
+import { SdkgenError, SdkgenErrorWithData } from "./error";
 
 interface ErrClasses {
-  [className: string]: typeof SdkgenError;
+  [className: string]: { new (message: string, data: any): SdkgenErrorWithData<any> | SdkgenError };
 }
 
 export class SdkgenHttpClient {
@@ -89,10 +89,18 @@ export class SdkgenHttpClient {
       const errClass = this.errClasses[error.type];
 
       if (errClass) {
-        throw new errClass(error.message);
-      } else {
-        throw new this.errClasses.Fatal(`${error.type}: ${error.message}`);
+        const errorJson = this.astJson.errors.find(err => (Array.isArray(err) ? err[0] === error.type : err === error.type));
+
+        if (errorJson) {
+          if (Array.isArray(errorJson)) {
+            throw new errClass(error.message, decode(this.astJson.typeTable, `${errClass.name}.data`, errorJson[1], error.data));
+          } else {
+            throw new errClass(error.message, undefined);
+          }
+        }
       }
+
+      throw new this.errClasses.Fatal(`${error.type}: ${error.message}`, undefined);
     });
 
     return decode(this.astJson.typeTable, `${functionName}.ret`, func.ret, encodedRet);

browser-runtime/src/ast.ts
@@ -16,4 +16,5 @@ export type TypeDescription = string | string[] | { [name: string]: TypeDescript
 export interface AstJson {
   typeTable: TypeTable;
   functionTable: FunctionTable;
+  errors: Array<string | string[]>;
 }

browser-runtime/src/error.ts
@@ -1,13 +1,25 @@
 export abstract class SdkgenError extends Error {
-  constructor(message?: string) {
-    super(message);
-    this.name = this.constructor.name;
+  get type(): string {
+    return this.constructor.name;
   }
 
   public toJSON(): { message: string; type: string } {
     return {
       message: this.message,
-      type: this.constructor.name,
+      type: this.type,
+    };
+  }
+}
+export abstract class SdkgenErrorWithData<DataType> extends SdkgenError {
+  constructor(message: string, public data: DataType) {
+    super(message);
+  }
+
+  public toJSON(): { data: DataType; message: string; type: string } {
+    return {
+      data: this.data,
+      message: this.message,
+      type: this.type,
     };
   }
 }

csharp-generator/src/csharp-server.ts
@@ -95,9 +95,9 @@ namespace SdkgenGenerated
 `;
   for (const error of ast.errors) {
     code += `
-    public class ${error}Exception : SdkgenException
+    public class ${error.name}Exception : SdkgenException
     {
-        public ${error}Exception(string message, Exception? inner = null) : base("${error}", message, inner) { }
+        public ${error.name}Exception(string message, Exception? inner = null) : base("${error.name}", message, inner) { }
     }
 `;
   }

dart-generator/src/dart-client.ts
@@ -90,7 +90,9 @@ ${ast.operations
 
   code += `var _errTable = {\n`;
   for (const error of ast.errors) {
-    code += `  "${error}": (msg) => ${error}(msg),\n`;
+    const hasData = !(error.dataType instanceof VoidPrimitiveType);
+
+    code += `  "${error.name}": SdkgenErrorDescription("${error.dataType.name}", (msg, data) => ${error.name}(msg${hasData ? ", data" : ""})),\n`;
   }
 
   code += `};\n`;

dart-generator/src/helpers.ts
@@ -10,6 +10,7 @@ import {
   DateTimePrimitiveType,
   EmailPrimitiveType,
   EnumType,
+  ErrorNode,
   FloatPrimitiveType,
   HexPrimitiveType,
   HtmlPrimitiveType,
@@ -49,10 +50,6 @@ function generateConstructor(type: StructType): string {
   });
   str = str.concat(`${doubleSpace}});\n`);
   return str;
-}
-
-export function generateErrorClass(error: string): string {
-  return `class ${error} extends SdkgenError {\n  ${error}(msg) : super(msg);\n}\n`;
 }
 
 export function generateTypeName(type: Type): string {
@@ -102,6 +99,16 @@ export function generateTypeName(type: Type): string {
     default:
       throw new Error(`BUG: generateTypeName with ${type.constructor.name}`);
   }
+}
+
+export function generateErrorClass(error: ErrorNode): string {
+  if (error.dataType instanceof VoidPrimitiveType) {
+    return `class ${error} extends SdkgenError {\n  ${error}(String msg) : super(msg);\n}\n`;
+  }
+
+  const dataType = generateTypeName(error.dataType);
+
+  return `class ${error} extends SdkgenErrorWithData<${dataType}> {\n  ${error}(String msg, ${dataType} data) : super(msg, data);\n}\n`;
 }
 
 export function cast(value: string, type: Type): string {

dart-runtime/lib/http_client.dart
@@ -16,11 +16,17 @@ class SdkgenError implements Exception {
   SdkgenError(this.message);
 }
 
+class SdkgenErrorWithData<T> implements Exception {
+  String message;
+  T data;
+  SdkgenErrorWithData(this.message, this.data);
+}
+
 class SdkgenHttpClient {
   String baseUrl;
   Map<String, Object> typeTable;
   Map<String, FunctionDescription> fnTable;
-  Map<String, Function> errTable;
+  Map<String, SdkgenErrorDescription> errTable;
   String deviceId;
   Random random = Random.secure();
   BuildContext context;
@@ -32,9 +38,12 @@ class SdkgenHttpClient {
     return hex.encode(List<int>.generate(bytes, (i) => random.nextInt(256)));
   }
 
-  _throwError(String type, String message) {
-    var factory = errTable[type] == null ? errTable["Fatal"] : errTable[type];
-    throw Function.apply(factory, [message]);
+  _throwError(String type, String message, dynamic data) {
+    var description =
+        errTable[type] == null ? errTable["Fatal"] : errTable[type];
+    var decodedData =
+        decode(this.typeTable, "$type.data", description.dataType, data);
+    throw Function.apply(description.create, [message, decodedData]);
   }
 
   _deviceId() async {
@@ -117,8 +126,8 @@ class SdkgenHttpClient {
       var responseBody = jsonDecode(utf8.decode(response.bodyBytes));
 
       if (responseBody["error"] != null) {
-        throw _throwError(
-            responseBody["error"]["type"], responseBody["error"]["message"]);
+        throw _throwError(responseBody["error"]["type"],
+            responseBody["error"]["message"], responseBody["error"]["data"]);
       } else {
         return decode(
             typeTable, "$functionName.ret", func.ret, responseBody["result"]);
@@ -127,7 +136,7 @@ class SdkgenHttpClient {
       if (e is SdkgenError)
         throw e;
       else
-        throw _throwError("Fatal", e.toString());
+        throw _throwError("Fatal", e.toString(), null);
     }
   }
 }

commit e1d90d460508c2f79094d4a750cc02cf2f4d9f44

fix(browser-runtime): actually return result (broken by #133)

browser-runtime/src/http-client.ts
@@ -130,5 +130,11 @@ export class SdkgenHttpClient {
 
       throw new this.errClasses.Fatal(`${error.type}: ${error.message}`, undefined);
     });
+
+    const ret = decode(this.astJson.typeTable, `${functionName}.ret`, func.ret, encodedRet);
+
+    this.successHook(ret, functionName, args);
+
+    return ret;
   }
 }