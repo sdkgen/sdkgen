385e37e19e3fb641ca2585ab94bddb44d94e2c80
davidcpires <dcpires06@gmail.com>
2021-06-22 19:29:43 -0300

    feat: allow node client to pass extra args to request (#378)
    Co-authored-by: David Pires <davidcpires@Davids-MacBook-Pro-2.local>
    Co-authored-by: David Pires <davidcpires@MacBook-Pro.localdomain>
    Co-authored-by: Guilherme Bernal <guilherme@cubos.io>

--- node-runtime/src/http-client.ts
+++ node-runtime/src/http-client.ts
@@ -7,6 +7,7 @@ import { hostname } from "os";
 import { URL } from "url";
 
 import type { AstJson } from "@sdkgen/parser";
+import type { PartialDeep } from "type-fest";
 
 import type { Context } from "./context";
 import { decode, encode } from "./encode-decode";
@@ -27,7 +28,7 @@ export class SdkgenHttpClient {
     this.baseUrl = new URL(baseUrl);
   }
 
-  async makeRequest(ctx: Context | null, functionName: string, args: unknown): Promise<any> {
+  async makeRequest(ctx: PartialDeep<Context> | null, functionName: string, args: unknown): Promise<any> {
     const func = this.astJson.functionTable[functionName];
 
     if (!func) {
@@ -42,13 +43,13 @@ export class SdkgenHttpClient {
 
     const requestBody = JSON.stringify({
       args: encode(this.astJson.typeTable, `${functionName}.args`, func.args, args),
-      deviceInfo: ctx?.request ? ctx.request.deviceInfo : { id: hostname(), type: "node" },
+      deviceInfo: ctx?.request?.deviceInfo ? ctx.request.deviceInfo : { id: hostname(), type: "node" },
       extra: {
         ...extra,
         ...(ctx?.request ? ctx.request.extra : {}),
       },
       name: functionName,
-      requestId: ctx?.request ? ctx.request.id + randomBytes(6).toString("hex") : randomBytes(16).toString("hex"),
+      requestId: ctx?.request?.id ? ctx.request.id + randomBytes(6).toString("hex") : randomBytes(16).toString("hex"),
       version: 3,
     });
 
