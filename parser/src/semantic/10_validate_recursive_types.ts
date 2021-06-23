ca06dbfbea07fb992acbb81e1f1e776bcacf0780
Guilherme Bernal <guilherme@cubos.io>
2021-06-23 11:21:00 -0400

    fix: eslint

--- parser/src/semantic/10_validate_recursive_types.ts
+++ parser/src/semantic/10_validate_recursive_types.ts
@@ -1,14 +1,16 @@
-import { ArrayType, AstNode, OptionalType, TypeDefinition, TypeReference } from "../ast";
+import type { AstNode } from "../ast";
+import { ArrayType, OptionalType, TypeDefinition, TypeReference } from "../ast";
 import { SemanticError, Visitor } from "./visitor";
 
 export class ValidateRecursiveTypes extends Visitor {
   visit(node: AstNode): void {
     super.visit(node);
 
-    if (node instanceof TypeDefinition &&
+    if (
+      node instanceof TypeDefinition &&
       ((node.type instanceof TypeReference && node.type.name === node.name) ||
-      (node.type instanceof ArrayType && node.type.base.name === node.name) ||
-      (node.type instanceof OptionalType && node.type.base.name === node.name))
+        (node.type instanceof ArrayType && node.type.base.name === node.name) ||
+        (node.type instanceof OptionalType && node.type.base.name === node.name))
     ) {
       throw new SemanticError(`Type '${node.name}' at ${node.location} is recursive but is not an struct`);
     }
