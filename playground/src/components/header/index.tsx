538457b610664bf0f3e2e6cbe416f93bed53e11d
Yan Santana <yan.barreiro@cubos.io>
2021-09-23 16:29:36 -0400

    feat: add route on header

--- playground/src/components/header/index.tsx
+++ playground/src/components/header/index.tsx
@@ -1,5 +1,5 @@
 import type { IconProp } from "@fortawesome/fontawesome-svg-core";
-import { faClone, faCog } from "@fortawesome/free-solid-svg-icons";
+import { faClone, faCog, faDownload } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import classnames from "classnames";
 import { observer } from "mobx-react";
@@ -18,21 +18,25 @@ interface LinkInfo {
 const links: LinkInfo[] = [
   { icon: faClone, label: "Endpoints", to: "/playground" },
   { icon: faCog, label: "Configuration", to: "/playground/configuration" },
+  { icon: faDownload, label: "Download client", to: "/playground/downloads" },
 ];
 
 function Header() {
   const { routerStore } = React.useContext(RootStore);
+  const divider = <div className={s.divider} />;
 
   const activePath = routerStore.location.pathname;
   const linkElements = links.map(l => (
-    <Link key={l.to} to={l.to}>
-      <div className={classnames(s.link, l.to === activePath && s.active)}>
-        <FontAwesomeIcon size="xs" icon={l.icon} className={s.icon} />
-        {l.label}
-      </div>
-    </Link>
+    <>
+      <Link key={l.to} to={l.to}>
+        <div className={classnames(s.link, l.to === activePath && s.active)}>
+          <FontAwesomeIcon size="xs" icon={l.icon} className={s.icon} />
+          {l.label}
+        </div>
+      </Link>
+      {divider}
+    </>
   ));
-  const divider = <div className={s.divider} />;
 
   return (
     <div className={s.header}>
@@ -42,10 +46,7 @@ function Header() {
           <div className={s.title}>sdkgen Playground</div>
         </div>
       </div>
-      <div className={s.actions}>
-        {linkElements}
-        {divider}
-      </div>
+      <div className={s.actions}>{linkElements}</div>
     </div>
   );
 }
