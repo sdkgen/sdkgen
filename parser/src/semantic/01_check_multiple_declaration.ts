ff91cd226638bfe046f4e242fcd93fdeb0b293bf
viniciusth <viniciustht@gmail.com>
2021-10-01 00:16:25 -0300

    feat: prohibit generic types of having same name as declared types

--- parser/src/semantic/01_check_multiple_declaration.ts
+++ parser/src/semantic/01_check_multiple_declaration.ts
@@ -1,3 +1,4 @@
+import { Operation } from "..";
 import type { AstNode, Type } from "../ast";
 import { TypeDefinition } from "../ast";
 import { SemanticError, Visitor } from "./visitor";
@@ -17,5 +18,17 @@ export class CheckMultipleDeclarationVisitor extends Visitor {
     }
 
     super.visit(node);
+    if (node instanceof TypeDefinition || node instanceof Operation) {
+      // eslint-disable-next-line no-foreach/no-foreach
+      node.generics.forEach(generic => {
+        const previouslyDeclared = this.nameToType.get(generic);
+
+        if (previouslyDeclared !== undefined) {
+          throw new SemanticError(
+            `Generic type '${generic}' used in '${node.name}' at ${node.location} can't have same name as declared type '${generic}' at '${previouslyDeclared.location}'`,
+          );
+        }
+      });
+    }
   }
 }
