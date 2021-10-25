8db5ddf54fb9edf1542cc93bfbdf60e8aa4b7004
Douglas G <dgadelha@live.com>
2021-10-25 23:07:16 +0000

    fix: add missing index.js

---
+++ playground/index.d.ts
@@ -0,0 +1,2 @@
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export declare const PLAYGROUND_PUBLIC_PATH: string;
---
+++ playground/index.js
@@ -0,0 +1,5 @@
+// eslint-disable-next-line
+const path = require("path");
+
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PLAYGROUND_PUBLIC_PATH = path.join(__dirname, "./dist");
