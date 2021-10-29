8629efab81c1fbb02bad82bdc927b2758cdb2282
Guilherme Bernal <guilherme@cubos.io>
2021-10-29 14:12:27 +0000

    Revert "fix: ast typing on browser-runtime"
    This reverts commit f0bc8b3cc422afffa21dc23c71415f426716f37c.

--- browser-runtime/src/ast.ts
+++ browser-runtime/src/ast.ts
@@ -1,43 +1,22 @@
-export type TypeDescription = string | string[] | Record<string, string>;
+export type TypeDescription = string | string[] | { [name: string]: TypeDescription };
 
-export type TypeTable = Record<string, TypeDescription | undefined>;
-export type FunctionTable = Record<
-  string,
-  | {
-      args: Record<string, string>;
-      ret: TypeDescription;
-    }
-  | undefined
->;
+export interface TypeTable {
+  [name: string]: TypeDescription | undefined;
+}
 
-type AnnotationJson =
-  | {
-      type: "description";
-      value: string;
-    }
-  | {
-      type: "throws";
-      value: string;
-    }
-  | {
-      type: "hidden";
-      value: null;
-    }
-  | {
-      type: "rest";
-      value: {
-        bodyVariable: string | null;
-        headers: ReadonlyArray<[string, string]>;
-        method: string;
-        path: string;
-        pathVariables: readonly string[];
-        queryVariables: readonly string[];
-      };
-    };
+export interface FunctionTable {
+  [name: string]:
+    | {
+        args: {
+          [arg: string]: TypeDescription;
+        };
+        ret: TypeDescription;
+      }
+    | undefined;
+}
 
 export interface AstJson {
   typeTable: TypeTable;
   functionTable: FunctionTable;
   errors: Array<string | string[]>;
-  annotations: Record<string, AnnotationJson[] | undefined>;
 }
