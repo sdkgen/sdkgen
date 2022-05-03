b6680077caf4e62a3f7926d1433f596d73bdc569
Guilherme Bernal <guilherme@cubos.io>
2022-05-03 15:11:27 -0300

    Fix: Better handing of repeated identical types (#672)
    * fix: error out if there are multiple types with the same name, as those will generate duplicate classes/interfaces on the codegen phase.
    
    * fix: Don't allow the same exact type to be collected more than once if two references to it appear on the AST (for example, when it is inlined by a spread operator)
    
    * fix: when two identical types are found the copy need to be replaced by the AST node of the first type to ensure we don't end up with two types identical sharing the same name. Previously this was ignored.
    
    * fix: avoid collecting duplicate enum/structs, but also keep the original order unchanged.

--- parser/src/semantic/08_collect_struct_and_enum_types.ts
+++ parser/src/semantic/08_collect_struct_and_enum_types.ts
@@ -3,20 +3,21 @@ import { EnumType, StructType } from "../ast";
 import { Visitor } from "./visitor";
 
 export class CollectStructAndEnumTypesVisitor extends Visitor {
+  visited = new Set<AstNode>();
+
   visit(node: AstNode): void {
     super.visit(node);
+
+    if (this.visited.has(node)) {
+      return;
+    }
+
+    this.visited.add(node);
 
     if (node instanceof StructType) {
       this.root.structTypes.push(node);
     } else if (node instanceof EnumType) {
       this.root.enumTypes.push(node);
     }
-  }
-
-  process(): void {
-    super.process();
-
-    this.root.structTypes = [...new Set(this.root.structTypes)];
-    this.root.enumTypes = [...new Set(this.root.enumTypes)];
   }
 }
