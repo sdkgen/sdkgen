378e1df96e6deaaef6fa1ec9fb5316c71a4457b6
José Cleiton Correia <jcleitonbc@gmail.com>
2022-08-29 11:36:27 -0300

    feat(parser): add hasType helper

---
+++ parser/src/helpers.ts
@@ -0,0 +1,25 @@
+import type { AstRoot, Type, AstNode } from "./ast";
+import { Visitor } from "./semantic/visitor";
+
+class HasTypeVisitor extends Visitor {
+  found = false;
+
+  constructor(root: AstRoot, private type: typeof Type) {
+    super(root);
+  }
+
+  visit(node: AstNode): void {
+    if (node.constructor === this.type) {
+      this.found = true;
+    }
+
+    super.visit(node);
+  }
+}
+
+export function hasType(root: AstRoot, type: typeof Type) {
+  const visitor = new HasTypeVisitor(root, type);
+
+  visitor.process();
+  return visitor.found;
+}
