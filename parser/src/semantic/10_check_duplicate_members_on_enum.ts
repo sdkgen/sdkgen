8ec278fa300024ebf441ae7d479e59240db8dd6e
José Cleiton <jose.correia@cubos.io>
2022-12-08 17:12:01 -0300

    Check duplicate value on enum type (#742)
    * perf(dart-generator): use hashAllUnordered to generate hash code
    
    * fix(parser): check duplicate members on enum types

---
+++ parser/src/semantic/10_check_duplicate_members_on_enum.ts
@@ -0,0 +1,15 @@
+import type { AstNode } from "../ast";
+import { EnumType } from "../ast";
+import { SemanticError, Visitor } from "./visitor";
+
+export class CheckDuplicatedMembersOnEnumVisitor extends Visitor {
+  visit(node: AstNode): void {
+    super.visit(node);
+
+    if (node instanceof EnumType) {
+      if (node.values.length !== new Set(node.values.map(x => x.value)).size) {
+        throw new SemanticError(`Enum '${node.name}' at ${node.location} has duplicated members`);
+      }
+    }
+  }
+}
